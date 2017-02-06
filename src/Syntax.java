513f491065f7688fae333a7033a7ed59ec219425
Graeth <smithgar002@gmail.com>
2017-02-05 16:46:50 -0800

    Copies to clipboard

--- src/Syntax.java
+++ src/Syntax.java
@@ -1,27 +1,44 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
 /**
  * Created by Julee on 2/5/2017.
  */
 public class Syntax {
     public static final String packageName = "org.usfirst.team3238.robot";
-
-    public static void GenerateClassHeader(String className) {
-        System.out.println("package " + packageName + ";" +
-                      "\n\npublic class " + className + " {\n");
-    }
-    public static void PostNumPoints(String name, int num) {
-        System.out.println("public static final String " + name + " = " + num +";\n");
-    }
-    public static void newPointArray(String name) {
-        System.out.println("public static double[][] " + name + " = new double[][] {");
+    public String classString = "";
+    public Syntax() {
 
     }
-    public static void newPoint(double r, double rps, int s) {
-        System.out.println("{" + r + ", " + rps + ", " + s + "},");
+    public void toClipboard() {
+        StringSelection selection = new StringSelection(classString);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection, selection);
     }
-    public static void GenerateArrayEnder(double r, double rps, int s) {
-        System.out.println("{" + r + ", " + rps + ", " + s + "}};\n");
+    public void GenerateClassHeader(String className) {
+        classString+= "package " + packageName + ";" +
+                "\n\npublic class " + className + " {\n";
     }
-    public static void endClass() {
-        System.out.println("}");
+
+    public void PostNumPoints(String name, int num) {
+        classString += "public static final String " + name + " = " + num + ";\n";
+    }
+
+    public void newPointArray(String name) {
+        classString += "public static double[][] " + name + " = new double[][] {";
+
+    }
+
+    public void newPoint(double r, double rps, int s) {
+        classString += "{" + r + ", " + rps + ", " + s + "},";
+    }
+
+    public void GenerateArrayEnder(double r, double rps, int s) {
+        classString += "{" + r + ", " + rps + ", " + s + "}};\n";
+    }
+
+    public void endClass() {
+        classString += "}";
     }
 }
